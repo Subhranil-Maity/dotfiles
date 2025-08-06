5e3e3fca3fe996b1a707b209faa085dce7493501
Subhranil Maity <fightingdj1@gmail.com>
2025-08-06 15:05:49 +0530

    updated poweeshell and added winstow.toml

---
+++ .config/clink/starship.lua
@@ -0,0 +1,4 @@
+load(io.popen('starship init cmd'):read("*a"))()
+-- os.setenv('STARSHIP_CONFIG', "c:\\Users\\fight\\AppData\\local\\clink\\starship.toml")
+-- os.setenv('STARSHIP_CONFIG', ".\\starship.toml")
+-- os.execute("cls")
---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+. $env:USERPROFILE\dotfiles\.config\powershell\user_profile.ps1
+# . user_profile.ps1
